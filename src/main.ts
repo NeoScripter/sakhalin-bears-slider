8fd32ce6c5815a3ceb7d7157ed63c093f2bc251f
Ilya <sange0337@gmail.com>
2025-02-25 18:25:13 +0800

    finish styling

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
 import './style.css'
+
+
+
+
+
+export function accessDomElement<T extends Element>(
+  selector: string,
+  expectedElementType: new () => T,
+  parent: HTMLElement = document.body
+): T {
+  const element = parent.querySelector(selector);
+
+  if (!element) {
+      throw new Error(`Element not found: ${selector}`);
+  }
+
+  if (!(element instanceof expectedElementType)) {
+      throw new Error(
+          `Expected ${expectedElementType.name}, but found ${element.constructor.name}`
+      );
+  }
+
+  return element;
+}
